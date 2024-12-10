1bb8aaf1868b6426c62129a8feec2a356045ea36
Joshua <adrishpinto423@gmail.com>
2024-12-10 18:44:06 +0530

    added meta

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="description" content="WeFrame project" />
         <title>WeFrame</title>
         <link
           rel="preload"
