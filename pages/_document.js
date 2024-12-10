91dad70fa5bee1bef5f2a5615aeac0c6bd42e736
Joshua <adrishpinto423@gmail.com>
2024-12-10 19:40:59 +0530

    image

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
       <Head>
         <meta name="description" content="WeFrame project" />
         <title>WeFrame</title>
+        <link rel="preload" href="/logo.svg" as="image"></link>
         <link
           rel="preload"
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
