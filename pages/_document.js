cd06ac8a06e8fa1940edcfe9c1c60711c29ab4fc
Joshua <adrishpinto423@gmail.com>
2024-12-10 18:26:48 +0530

    font

--- pages/_document.js
+++ pages/_document.js
@@ -3,11 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
-        rel="stylesheet"
-      />
+      <Head>
+        <link
+          rel="preload"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
+          as="font"
+          type="font/woff2"
+          crossorigin="anonymous"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
